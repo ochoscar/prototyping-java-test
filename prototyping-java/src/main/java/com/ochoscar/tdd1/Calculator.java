051819b91ccfca3d916dc867db8d7ecf7448ee31
Oscar Andres Ochoa Villegas <ochoscar@gmail.com>
2025-01-26 12:26:38 -0500

    Initial tests

---
+++ prototyping-java/src/main/java/com/ochoscar/tdd1/Calculator.java
@@ -0,0 +1,7 @@
+package com.ochoscar.tdd1;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ prototyping-java/src/test/java/com/ochoscar/tdd1/CalculatorTest.java
@@ -0,0 +1,15 @@
+package com.ochoscar.tdd1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAddition() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(2, 3);
+        assertEquals(5, result);
+    }
+}
